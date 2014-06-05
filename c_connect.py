60c7ce8bc7f84459317bacfd22d4b9b95f5a4bb7
amb1s1 <amb1s1@gmail.com>
2014-06-05 19:33:11 -0400

    Adding some notes and comments
    Signed-off-by: amb1s1 <amb1s1@gmail.com>

--- c_connect.py
+++ c_connect.py
@@ -1,4 +1,25 @@
-#!/user/bin/python
+''' This script is specially design for cisco router and switches. This
+is going to be use to ssh into cisco devices.  This script is base on Pexpect
+and to be able to use it, you would need a good knowledge of pexpect.
+
+PEXPECT LICENSE
+
+    This license is approved by the OSI and FSF as GPL-compatible.
+        http://opensource.org/licenses/isc-license.txt
+
+    Copyright (c) 2014, Noah Spurrier <noah@noah.org>
+    PERMISSION TO USE, COPY, MODIFY, AND/OR DISTRIBUTE THIS SOFTWARE FOR ANY
+    PURPOSE WITH OR WITHOUT FEE IS HEREBY GRANTED, PROVIDED THAT THE ABOVE
+    COPYRIGHT NOTICE AND THIS PERMISSION NOTICE APPEAR IN ALL COPIES.
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+'''
 import sys
 import pexpect
 
@@ -121,3 +142,11 @@ def close(child):
         return 'Getting EOF Error'
     elif 1 == 2:
         return 'Timeout'
+
+
+
+
+
+
+
+
